f2799e2e91c0ac817c5846afeb73e9f936094e5e
Kavya770 <98035868+Kavya770@users.noreply.github.com>
2022-11-28 15:54:07 +0530

    n=int(input()) l=list(map(int,input().split())) if len(l)!=n:     for i in range(n-len(l)):         l.append(0) lst=[] for i in l:     if i not in lst and i%2==0:         lst.append(i) print(sum(lst))

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=list(map(int,input().split()))
+if len(l)!=n:
+    for i in range(n-len(l)):
+        l.append(0)
+lst=[]
+for i in l:
+    if i not in lst and i%2==0:
+        lst.append(i)
+print(sum(lst))
